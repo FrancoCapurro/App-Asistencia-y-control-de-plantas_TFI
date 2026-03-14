8905041cc42ec6fa226a9253b27cbb218566568b
FrancoCapurro <franco.capurro@hotmail.com>
2026-03-14 11:27:04 -0300

    Agrego proyecto android

---
+++ AppAndroid/app/src/test/java/frgp/utn/edu/ar/equipo17/app_asistencia_y_control_de_plantas/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package frgp.utn.edu.ar.equipo17.app_asistencia_y_control_de_plantas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
